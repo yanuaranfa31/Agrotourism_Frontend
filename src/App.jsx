55d6ff1cd6d5c2b4b35fea74009583ad3cd08a7b
Yanuar <22sa11a014@mhs.amikompurwokerto.ac.id>
2024-12-16 11:38:26 +0700

    mendaftarkan routes

--- src/App.jsx
+++ src/App.jsx
@@ -12,29 +12,40 @@ import Login from './pages/Login';
 import LupaPassword from './pages/LupaPassword';
 import BookingForm from './pages/BookingForm';
 import Confirmation from './pages/Confirmation'
+import Dashboard from './pages/Dashboard';
+import ApprovalAdmin from './pages/ApprovalAdmin';
+import KuotaAdmin from './pages/KuotaAdmin';
+import ProtectedRoute from './pages/ProtectedRoute';
+import ProtectedAdminRoute from './pages/ProtectedAdminRoute';
 
 function App() {
   return (
     <AuthProvider>
-    <BrowserRouter>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/lupa-password" element={<LupaPassword />} />
+      <BrowserRouter>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/lupa-password" element={<LupaPassword />} />
 
-          <Route path="/" element={<Beranda />} />
-          <Route path="/cek-kuota" element={<CekKuota />} />
-          <Route path="/destinasi-wisata" element={<DestinasiWisata />} />
-          <Route path="/panduan-booking" element={<PanduanBooking />} />
-          <Route path="/penginapan" element={<Penginapan />} />
+            <Route path="/" element={<Beranda />} />
+            <Route path="/cek-kuota" element={<CekKuota />} />
+            <Route path="/destinasi-wisata" element={<DestinasiWisata />} />
+            <Route path="/panduan-booking" element={<PanduanBooking />} />
+            <Route path="/penginapan" element={<Penginapan />} />
 
-          <Route path="/booking" element={<BookingForm />} />
-          <Route path="/confirmation" element={<Confirmation />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+            <Route path="/booking" element={<BookingForm />} />
+            <Route path="/confirmation" element={<Confirmation />} />
+
+            <Route path="/" element={<ProtectedAdminRoute />}>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/kuota-admin" element={<KuotaAdmin />} />
+              <Route path="/approval-admin" element={<ApprovalAdmin />} />
+            </Route>
+          </Routes>
+        </div>
+      </BrowserRouter>
     </AuthProvider>
   );
 }
