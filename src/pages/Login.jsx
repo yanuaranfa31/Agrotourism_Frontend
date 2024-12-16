a0411d764530934af39fd5230da013d5e0b618b3
Yanuar <22sa11a014@mhs.amikompurwokerto.ac.id>
2024-12-16 11:37:01 +0700

    update admin

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,53 +1,48 @@
 import { Link } from 'react-router-dom';
-import React, { useState, useContext } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { AuthContext } from "../context/AuthContext";
+import React, { useContext, useState } from 'react';
+import { useAuth } from '../context/AuthContext';
 
 const Login = () => {
-    const { login } = useContext(AuthContext);
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
-    const navigate = useNavigate();
+    const { user, error, loading, login } = useAuth();
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
+    });
+    const [errorPage, setError] = useState(null);
 
-    const handleLogin = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if (!email || !password) {
-            setErrorMessage('Email dan password harus diisi.');
-            return;
-        }
-
-        if (email === 'test@example.com' && password === 'password123') {
-            login();
-            navigate('/');
-        } else {
-            setErrorMessage('Email atau password salah.');
+        try {
+            const data = await login(formData)
+            window.location.href = '/';        
+            
+        } catch (error) {
+            setError(error.message);
         }
     };
 
     return (
         <div className="flex justify-center items-center h-screen bg-gray-100">
-            <form onSubmit={handleLogin} className="w-full max-w-md bg-white p-8 rounded shadow-lg">
+            <form onSubmit={handleSubmit} className="w-full max-w-md bg-white p-8 rounded shadow-lg">
                 <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
 
-                {errorMessage && (
-                    <div className="mb-4 text-red-500 text-center">{errorMessage}</div>
+                {!error && (
+                    <div className="mb-4 text-red-500 text-center">{error}</div>
                 )}
-
                 <div className="mb-4">
                     <label className="block text-gray-700 mb-2" htmlFor="email">
                         Email
                     </label>
                     <input
                         type="email"
-                        id="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
+                        placeholder="Email"
+                        value={formData.email}
+                        onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                         className="w-full px-4 py-2 border border-gray-300 rounded"
-                        required
                     />
                 </div>
+
 
                 <div className="mb-6">
                     <label className="block text-gray-700 mb-2" htmlFor="password">
@@ -55,20 +50,18 @@ const Login = () => {
                     </label>
                     <input
                         type="password"
-                        id="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
+                        placeholder="Password"
+                        value={formData.password}
+                        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
                         className="w-full px-4 py-2 border border-gray-300 rounded"
-                        required
                     />
                 </div>
 
                 <button
                     type="submit"
                     className="w-full bg-blue-500 text-white py-2 rounded hover:bg-blue-700 transition-all"
-                >
-                    Login
-                </button>
+                >Login</button>
+
 
                 <div className="mt-4 text-center">
                     <Link to="/lupa-password" className="text-blue-500 hover:underline">
